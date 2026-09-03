0f060a8bbea54434ef7b43691ece60baf4e8bc36
eskokado <eskokado@gmail.com>
2026-09-03 13:08:59 -0300

    refactor(product): usar changePrice no update e ajustar configuração de dados
    Atualiza ProductManagementApplicationService para usar changePrice e
    ajusta application.yaml para carregar dados de teste menores.

--- src/main/java/com/eskcti/algashop/product/catalog/application/product/management/ProductManagementApplicationService.java
+++ src/main/java/com/eskcti/algashop/product/catalog/application/product/management/ProductManagementApplicationService.java
@@ -48,9 +48,8 @@ public class ProductManagementApplicationService {
         product.setName(input.getName());
         product.setBrand(input.getBrand());
         product.setDescription(input.getDescription());
-        product.setRegularPrice(input.getRegularPrice());
-        product.setSalePrice(input.getSalePrice());
         product.setEnabled(input.getEnabled());
+        product.changePrice(input.getRegularPrice(), input.getSalePrice());
     }
 
     private Product findProduct(UUID productId) {
